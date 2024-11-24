03f27997330ca64085966c96970133e900bd662a
Chanho Kim <nonamed19@naver.com>
2024-11-24 13:19:02 +0900

    241124 BACK ACCOUNTS FRONT SAVINGS NEWS

--- final-pjt-back/finance_recommendation/urls.py
+++ final-pjt-back/finance_recommendation/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.views.generic import TemplateView
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('myadmin/', admin.site.urls),
@@ -31,4 +34,4 @@ urlpatterns = [
     path('stocks/', include('stocks.urls')),
     path('economics/', include('economics.urls')),
     path('chats/', include('chats.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
